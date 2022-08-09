55e527337e497a2c7c3bba7d1a4b2882ed026289
AlfieRan <alfie.ranstead@outlook.com>
2022-08-09 15:21:02 +0100

    fixed spelling mistakes + added some testing boilerplate

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from sbot import Robot
+from sbot.env import ConsoleEnvironment, HardwareEnvironment
 import movement
 import time
 
-# constants - robot
-ROBOT = Robot(wait_start=False) # create the robot object
-ARDUINO = ROBOT.arduino     # get the arduino object
+# configuration constants - change these but not others unless you know what you're doing
+TESTING=False
+
+
+# code based constants - don't change these unless you know what you're doing
+ENVIRONMENT = HardwareEnvironment if not TESTING else ConsoleEnvironment # environment to run in
+ROBOT = Robot(wait_start=False, debug=True, ) # create the robot object
+# ARDUINO = ROBOT.arduino     # get the arduino object
 MOVEMENT = movement.get_movement(ROBOT) # get the movement object
 
 def setup():
@@ -16,7 +22,7 @@ def init():
     print("Launching...")
     setup() # stuff to be ran before bot launches
     ROBOT.wait_start() # start the robot
-    
+
     test() # run the test function
     return
 
@@ -24,10 +30,14 @@ def init():
 def test():
     print("Testing...")
 
-    for i in range(0, 50):
+    for i in range(0, 5):
         MOVEMENT.forward() # move forward
         time.sleep(1000)   # wait 1 second
+        MOVEMENT.stop()  # stop
+        time.sleep(1000)   # wait 1 second
         MOVEMENT.backward() # move backward
+        time.sleep(1000)   # wait 1 second
+        MOVEMENT.stop()  # stop
         time.sleep(1000)   # wait 1 second
 
     return
